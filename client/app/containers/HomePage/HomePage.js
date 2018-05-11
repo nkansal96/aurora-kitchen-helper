e6bf6771f2a709a5b8cd7c7d7251d8810439d664
Hao Nguyen <haolnguyen7@gmail.com>
2018-05-10 22:33:56 -0700

    frontend is up

--- client/app/containers/HomePage/HomePage.js
+++ client/app/containers/HomePage/HomePage.js
@@ -4,13 +4,13 @@
  * This is the first thing users see of our App, at the '/' route
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import ReposList from 'components/ReposList';
-import './style.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import "./style.scss";
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class HomePage extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
   /**
    * when initial state username is not null, submit the form to load repos
    */
@@ -25,36 +25,40 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     const reposListProps = {
       loading,
       error,
-      repos,
+      repos
     };
 
     return (
       <article>
         <Helmet>
           <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
+          <meta name="description" content="Kitchen Helper App" />
         </Helmet>
         <div className="home-page">
           <section className="centered">
-            <h2>Start your next react project in seconds</h2>
-            <p>A minimal <i>React-Redux</i> boilerplate with all the best practices</p>
+            <h1>Kitchen Helper App</h1>
           </section>
-          <section>
-            <h2>Try me!</h2>
-            <form onSubmit={this.props.onSubmitForm}>
-              <label htmlFor="username">
-              Show Github repositories by
-                <span className="at-prefix">@</span>
-                <input
-                  id="username"
-                  type="text"
-                  placeholder="flexdinesh"
-                  value={this.props.username}
-                  onChange={this.props.onChangeUsername}
-                />
-              </label>
-            </form>
-            <ReposList {...reposListProps} />
+          <section className="inner-section">
+            <h2>Timer</h2>
+            <p>
+              Chicken &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+              &emsp; &emsp; &emsp; &emsp; 10:43 Remaining
+            </p>
+            <p>
+              Veggies &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+              &emsp; &emsp; &emsp; &emsp; 5:12 Remaining
+            </p>
+          </section>
+          <section className="inner-section">
+            <h2>Audio</h2>
+            <p> Now Playing: Cooking with Winter Vegatbles</p>
+          </section>
+          <section className="inner-section">
+            <h2>Appliances</h2>
+            <p>
+              Oven &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+              &emsp; &emsp; &emsp; &emsp; On
+            </p>
           </section>
         </div>
       </article>
@@ -64,15 +68,9 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
 
 HomePage.propTypes = {
   loading: PropTypes.bool,
-  error: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.bool,
-  ]),
-  repos: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.bool,
-  ]),
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
   onSubmitForm: PropTypes.func,
   username: PropTypes.string,
-  onChangeUsername: PropTypes.func,
+  onChangeUsername: PropTypes.func
 };
