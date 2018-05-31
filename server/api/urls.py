8fe43ecde63edd26d5383b8b85dd297101339967
Hao Nguyen <haolnguyen7@gmail.com>
2018-05-30 17:07:48 -0700

    init commit

--- server/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from api.views import TimerView
-
-urlpatterns = [
-    url(r'^timer/?$', TimerView.as_view(), name='timer'),
-    url(r'^audio/?$', TimerView.as_view(), name='audio'),
-    url(r'^device/?$', TimerView.as_view(), name='device'),
-]
